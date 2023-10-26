dd02c692b534a348a17317fb631989c4b5ad3b66
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2023-10-26 18:12:11 +0000

    Done

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,24 @@
-document.querySelector('form').addEventListener('submit', async (e) => {
-      e.preventDefault();
-        const code = document.querySelector('textarea').value;
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const code = document.querySelector("textarea").value;
 
-          if (code.trim() === '') {
-              alert('Please enter code before generating a screenshot.');
-                  return;
-                    }
+  if (code.trim() === "") {
+    alert("Please enter code before generating a screenshot.");
+    return;
+  }
 
-                      const response = await fetch(`/generate?code=${encodeURIComponent(code)}`);
-                        if (response.ok) {
-                            const blob = await response.blob();
-                                const url = URL.createObjectURL(blob);
-                                    document.querySelector('#screenshot').src = url;
-                                      } else {
-                                          alert('An error occurred while generating the screenshot.');
-                                            }
-                                            });
+  const response = await fetch('http://localhost:3000/generate',
+  { method : 'POST',
+    headers : {
+      'Content-Type' : 'application/x-www-form-urlencoded',
+    },
+    body: `code=${encodeURIComponent(code)}`,
+  });
+  if (response.ok) {
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    document.querySelector("#screenshot").src = url;
+  } else {
+    alert("An error occurred while generating the screenshot.");
+  }
+});
--- server/server.js
+++ server/server.js
@@ -1,41 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const port = process.env.PORT || 3000;
+const bodyparser = require("body-parser")
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(express.static("frontend"));
 
-app.use(express.static('frontend'));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "frontend" });
+});
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: 'frontend' });
-  });
+app.post("/generate", async (req, res) => {
+  const { code } = req.query;
 
-  app.post('/generate', async (req, res) => {
-    const { code } = req.query;
-      
-        if (!code) {
-            res.status(400).send('Code input is required.');
-                return;
-                  }
+  if (!code) {
+    res.status(400).send("Code input is required.");
+    return;
+  }
 
-                    try {
-                        const browser = await puppeteer.launch();
-                            const page = await browser.newPage();
-                                await page.setContent(`<pre><code>${code}</code></pre>`);
-                                    const screenshot = await page.screenshot();
-                                        
-                                            res.writeHead(200, {
-                                                  'Content-Type': 'image/png',
-                                                        'Content-Length': screenshot.length,
-                                                            });
-                                                                res.end(screenshot);
-                                                                    
-                                                                        await browser.close();
-                                                                          } catch (error) {
-                                                                              console.error(error);
-                                                                                  res.status(500).send('Error generating screenshot.');
-                                                                                    }
-                                                                                    });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(`<pre><code>${code}</code></pre>`);
+    const screenshot = await page.screenshot();
 
-                                                                                    app.listen(port, () => {
-                                                                                      console.log(`Server is running on port ${port}`);
-                                                                                      });
+    res.writeHead(200, {
+      "Content-Type": "image/png",
+      "Content-Length": screenshot.length,
+    });
+    res.end(screenshot);
+
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error generating screenshot.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+});
